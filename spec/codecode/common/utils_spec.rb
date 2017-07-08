c597d2199dad3a7598f4bf469f9f356b08c5eb03
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2017-07-08 06:38:30 -0300

    Added new Bang Method symbolize keys...

--- spec/codecode/common/utils_spec.rb
+++ spec/codecode/common/utils_spec.rb
@@ -37,14 +37,60 @@ RSpec.describe CodeCode::Common::Utils do
         'fourth_key' => :test
     }
 
-    100.times do |n|
-      object["nested object #{n}"] = other_object
-    end
+    100.times {|n| object["nested object #{n}"] = other_object}
 
     object = CodeCode::Common::Utils::Hash.symbolize_keys object
     object.each{ |key, value|
       expect(key).to be_an_instance_of(Symbol)
     }
+  end
+
+  it 'should convert object with string keys to symbol keys - nested level BANG METHOD' do
+    object = {
+        'first_key' => 'first_value',
+        'second_key' => 'second_value',
+        'third_key' => 'third_value'
+    }
+
+    other_object = {
+        'first_key' => 'string',
+        'second_key' =>  2.0,
+        'third_key' => 5,
+        'fourth_key' => :test
+    }
+
+    100.times {|n| object["nested object #{n}"] = other_object}
+
+    CodeCode::Common::Utils::Hash.symbolize_keys! object
+
+    object.each{ |key, value|
+      expect(key).to be_an_instance_of(Symbol)
+    }
+
+  end
+
+  it 'should NOT convert object with string keys to symbol keys - nested level Without BANG METHOD' do
+    object = {
+        'first_key' => 'first_value',
+        'second_key' => 'second_value',
+        'third_key' => 'third_value'
+    }
+
+    other_object = {
+        'first_key' => 'string',
+        'second_key' =>  2.0,
+        'third_key' => 5,
+        'fourth_key' => :test
+    }
+
+    100.times {|n| object["nested object #{n}"] = other_object}
+
+    CodeCode::Common::Utils::Hash.symbolize_keys object
+
+    object.each{ |key, value|
+      expect(key).not_to be_an_instance_of(Symbol)
+    }
+
   end
 
 end
