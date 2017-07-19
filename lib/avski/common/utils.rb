9ed7816194a6b9bec8a980fcfaf82ba310eaf6b0
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2017-07-18 21:10:35 -0300

    Added new Validation method and Exceptions

--- lib/avski/common/utils.rb
+++ lib/avski/common/utils.rb
@@ -52,28 +52,28 @@ module Avski
         def self.select(hash, keys)
           hash.select { |k, v| keys.include?(k) }
         end
+      end
 
-        # Utility methods for Validation
-        module Validation
+      # Utility methods for Validation
+      module Validation
 
-          # Check if fields used in method params have their values
-          # empty or keys are missing
-          # @param [Array[Symbol]] required_fields array of symbols
-          # @param [Object] params
-          def self.check_fields(required_fields, params)
-            required_fields, missing, empty, null = required_fields, [], [], []
-            required_fields.each { |key|
-              missing.push(key) unless params.keys.include?(key)
-              empty.push(key) if params[key.to_sym].eql?('')
-              null.push(key) if params[key.to_sym].nil?
-            }
+        # Check if fields used in method params have their values
+        # empty or keys are missing
+        # @param [Array[Symbol]] required_fields array of symbols
+        # @param [Object] params
+        def self.check_fields(required_fields, params)
+          required_fields, missing, empty, null = required_fields, [], [], []
+          required_fields.each { |key|
+            missing.push(key) unless params.keys.include?(key)
+            empty.push(key) if params[key.to_sym].eql?('')
+            null.push(key) if params[key.to_sym].nil?
+          }
 
-            raise UnknownFieldException.new("Missing field(s) -> #{missing.join(', ')}.", 400) if missing.length > 0
+          raise UnknownFieldException.new("Missing field(s) -> #{missing.join(', ')}.", 400) if missing.length > 0
 
-            raise EmptyFieldException.new("Empty field(s) -> #{empty.join(', ')}.", 400) if empty.length > 0
+          raise EmptyFieldException.new("Empty field(s) -> #{empty.join(', ')}.", 400) if empty.length > 0
 
-            raise NullFieldException.new("Null field(s) -> #{null.join(', ')}.", 400) if null.length > 0
-          end
+          raise NullFieldException.new("Null field(s) -> #{null.join(', ')}.", 400) if null.length > 0
         end
       end
     end
